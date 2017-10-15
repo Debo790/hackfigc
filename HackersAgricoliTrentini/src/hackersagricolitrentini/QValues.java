165c4f6a59d585113a0a8b0744b0c9145289c2cc
dadotrent <davide.trnt@gmail.com>
2017-10-15 02:45:04 +0200

    Create QValues.java

---
+++ HackersAgricoliTrentini/src/hackersagricolitrentini/QValues.java
@@ -0,0 +1,14 @@
+package hackersagricolitrentini;
+
+public class QValues {
+
+  int qualifier_id;
+  int outcome;
+  
+  
+  public QValues(int qualifier_id, int outcome) {
+    this.qualifier_id = qualifier_id;
+    this.outcome = outcome;
+  }
+
+}
